fe890230a46967bafae46c4f0b5560d0b61e7714
Alvaro Paiva <153731229+AlvaroPaiva2@users.noreply.github.com>
2024-05-19 15:49:12 -0300

    domingo

--- 2.py
+++ 2.py
@@ -14,11 +14,70 @@
 # else:
 #     print(f'{dia} dia, {hrs}h, {mnts}m e {secs} segundos')
 
-a = int(input("Qual o valor de a? "))
-b = int(input("Qual o valor de b? "))
-aux = a
-a = b
-b = aux
-print(a)
-print(b
+# a = int(input("Qual o valor de a? "))
+# b = int(input("Qual o valor de b? "))
+# aux = a
+# a = b
+# b = aux
+# print(a)
+# print(
+
+# dia = int(input('Digite a quantidade de dias: '))
+#
+# match dia:
+#     case dia if 1 <= dia <= 30:
+#         print(f'um mes!')
+#     case _:
+#         print(f'fudeu')
+#
+#
+# print('pergunta 01')
+# answer = input('Com quantos paus se faz uma canoa?\n'
+#                'a) 3\n'
+#                'b) 4\n'
+#                'c) 5\n'
+#                'd) 6\n')
+# match answer:
+#     case 'a':
+#         print('Resposta errada')
+#     case 'b':
+#         print('Resposta correta')
+#     case 'c':
+#         print('Resposta errada')
+#     case 'd':
+#         print('ta de sacanagem?')
+#     case _:
+#         print('Resposta invalida')
+
+nome = input('Digite seu nome: ')
+gostaNome = str(input(f'Olá, {nome.capitalize()}! Você gosta do seu nome? '))
+if gostaNome == 'sim' or 'Sim' or 'SIM' or 's' or 'S':
+    print('Que bom!')
+else:
+    print('Que pena!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ rascunho.py
@@ -0,0 +1,3 @@
+tempC = float(input('Temperatura em c: '))
+tempF = ((9*tempC)/5)+32
+print(f'A temperatura em farenait é: {tempF}')
